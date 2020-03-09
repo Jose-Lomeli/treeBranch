7b4d7a065b16afdcf333dd1723bcc5a0ee55f43b
91084699 <91084699@mvusd.net>
2020-03-09 13:28:28 -0700

    Thanks Jose

--- src/TreeBranchTrunk.java
+++ src/TreeBranchTrunk.java
@@ -2,6 +2,8 @@ public class TreeBranchTrunk
 {
     public static void main(String[]args)
     {
-        System.out.println("the trees r dying> ut mr beast is clutch");
+        System.out.println("the trees r dyin");
+        System.out.println("Mr beast planted 20,000,000");
+        System.out.println("Pine trees");
     }
 }//end driver
